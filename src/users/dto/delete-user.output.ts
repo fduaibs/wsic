e1a98767ada010691082ca0ba2a65899bba3b6f8
Felipe Duaibs <felipe.duaibs@hotmail.com>
2022-03-04 04:25:41 -0300

    fix: delete-user output decorators

--- src/users/dto/delete-user.output.ts
+++ src/users/dto/delete-user.output.ts
@@ -1,15 +1,17 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { IsBoolean, IsNotEmpty, IsObject, IsOptional } from 'class-validator';
 import { User } from '../entities/user.entity';
 
-@InputType()
+@ObjectType()
 export class DeleteUserOutput {
   @IsBoolean()
   @IsNotEmpty({ message: 'The field "deleted" cannot be empty' })
+  @Field()
   deleted: boolean;
 
   @IsObject()
   @IsNotEmpty({ message: 'The field "user" cannot be empty' })
   @IsOptional()
+  @Field({ nullable: true })
   user?: User;
 }
